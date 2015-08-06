7114d371527eb83a8d38c72ae595c4766f06b4be
Zimmer-web <getarun@users.noreply.github.com>
2015-08-06 13:43:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ openssl req -new -x509 \
     -keyout server.pem -out server.pem \
     -days 365 -nodes
 ##copies key
-sudo cp server.pem /etc/lighttpd/ssl/
+sudo mv server.pem /etc/lighttpd/ssl/
 ## changes permissions to be only writeable for root (which is okay, because server gets started as root)
 sudo chown root:root /etc/lighttpd/ssl/server.pem
 chmod 400 /etc/lighttpd/ssl/server.pem
