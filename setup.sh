8b01eca88a698bfba774e3a38c9b4dead6deb2ee
Zimmer-web <getarun@users.noreply.github.com>
2015-08-06 12:45:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,20 +11,20 @@ sudo chown root:root /etc/lighttpd/ssl/server.pem
 chmod 400 /etc/lighttpd/ssl/server.pem
 
 ############################ check if ssl is compiled ######################
-download the lighttpd tarball from http://www.lighttpd.net/download and extract it with
-``tar zxvf lighttpd-1.4.20.tar.gz``
+#download the lighttpd tarball from http://www.lighttpd.net/download and extract it with
+#tar zxvf lighttpd-1.4.20.tar.gz
 
-Enter into the extracted folder and compile with these configuration flags: ::
+#Enter into the extracted folder and compile with these configuration flags: ::
 
-    ``--with-openssl --with-openssl-libs=/usr/lib``
+#    --with-openssl --with-openssl-libs=/usr/lib
 
-Remember to change _ --with-openssl-libs= _ to the folder where your openssl libraries are installed into.
+#Remember to change _ --with-openssl-libs= _ to the folder where your openssl libraries are installed into.
 
-Once compiled, run make and make install. If lighty has successfully compiled SSL the command
+#Once compiled, run make and make install. If lighty has successfully compiled SSL the command
 
-    ``lighttpd -v``
+#    lighttpd -v
 
-Should display (Keep in mind that this new lighty version now has (ssl) after lightys name)
+#Should display (Keep in mind that this new lighty version now has (ssl) after lightys name)
 
-    lighttpd-1.4.11 (ssl) - a light and fast webserver
-    Build-Date: Sep 1 2006 19:09:15
+#    lighttpd-1.4.11 (ssl) - a light and fast webserver
+#    Build-Date: Sep 1 2006 19:09:15
