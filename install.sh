5c80b0c49a3ee9788f78a681f05077cf6be36542
Zimmer-pi <zimmer@jzo.de>
2015-08-06 16:49:46 +0000

    changed typos

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 ##################################### Server installation #####################################
 ## installs lighttpd with ssl support
-sudo apt-get install openssl libssl-dev
+sudo apt-get install openssl libssl-dev lighttpd
 ## follow http://redmine.lighttpd.net/projects/lighttpd/wiki/Docs_SSL#Self-Signed-Certificates
-mkdir ~/downloads/
-cd ~/downloads/
-wget http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.36.tar.gz
-tar zxvf lighttpd-1.4.36.tar.gz
-cd lighttpd-1.4.36
-./configure --with-openssl --with-openssl-libs=/usr/lib
-make
-sudo make install
-cd ~
+#mkdir ~/downloads/
+#cd ~/downloads/
+#wget http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.36.tar.gz
+#tar zxvf lighttpd-1.4.36.tar.gz
+#cd lighttpd-1.4.36
+#./configure --with-openssl --with-openssl-libs=/usr/lib
+#make
+#sudo make install
+#cd ~
 lighttpd -v
 ##################################### Key installation #####################################
 ###creates new key
@@ -22,6 +22,6 @@ openssl req -new -x509 \
 sudo mv server.pem /etc/lighttpd/
 ## changes permissions to be only writeable for root (which is okay, because server gets started as root)
 sudo chown root:root /etc/lighttpd/server.pem
-chmod 400 /etc/lighttpd/server.pem
+sudo chmod 400 /etc/lighttpd/server.pem
 ##################################### Copies configuration file from repo
-sudo cp ~/git-working-dir/lighttpd-config/lighttpd.config /etc/lighttpd/
+sudo cp ~/git-working-dir/lighttpd-config/lighttpd.conf /etc/lighttpd/
